b9826221cbcacc18f43d1807e9020c1eca825985
David Yoon <davidyoon@hanwha.com>
2024-01-23 08:27:56 +0900

    Initial Commit

---
+++ NavigationDeepLink/NavigationDeepLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDeepLinkApp.swift
+//  NavigationDeepLink
+//
+//  Created by Davidyoon on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationDeepLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
